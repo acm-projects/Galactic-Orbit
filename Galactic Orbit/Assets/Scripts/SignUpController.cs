c54ee6f605ab262e28ef1f2aeffab9e3f83a7688
Hadent <hadentd@gmail.com>
2025-10-16 23:24:10 -0500

    Fixing bg sprite resolution

--- Galactic Orbit/Assets/Scripts/SignUpController.cs
+++ Galactic Orbit/Assets/Scripts/SignUpController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections;
 
 public class SignUpController : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class SignUpController : MonoBehaviour
     private TextField usernameField;
     private TextField passwordField;
     private Button submitButton;
+    public GameObject LogInPanel;
 
     void OnEnable()
     {
@@ -21,9 +23,13 @@ public class SignUpController : MonoBehaviour
         if (emailField ==null) Debug.LogError("EmailInput field not found!");
         if (usernameField ==null) Debug.LogError("UsernameInput field not found!");
         if (passwordField ==null) Debug.LogError("PasswordInput field not found!");
-        if (submitButton ==null) Debug.LogError("SubmitButton button not found!");
+        if (submitButton == null) Debug.LogError("SubmitButton button not found!");
 
         // Hook up the button’s clicked event
+        
+        var switchButton = root.Q<Button>("SwitchScreenButton");
+        switchButton.clicked += switchPanels;
+
         submitButton.clicked += OnSubmit;
     }
 
@@ -38,7 +44,24 @@ public class SignUpController : MonoBehaviour
         passwordField.value = "";
 
         Debug.Log($"Submitted! Email: {email}, Username: {username}, Password: {password}");
-    
+        StartCoroutine(SubmitSection());
         // Here you could add validation, send to server, etc.
+    }
+    
+     private void switchPanels()
+    {
+        LogInPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    IEnumerator SubmitSection()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var colorSection = root.Q<VisualElement>("Pressing");
+        colorSection.AddToClassList("selected");
+
+        yield return new WaitForSeconds(0.5f);
+
+        colorSection.RemoveFromClassList("selected");
+
     }
 }
